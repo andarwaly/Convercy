27260c5427705241e3baeab584366ac678be19c0
Dzaky Waly <andarwaly@gmail.com>
2024-09-10 16:28:49 +0700

    Initial Commit

---
+++ Convercy/ConvercyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConvercyApp.swift
+//  Convercy
+//
+//  Created by Muhammad Dzaky on 10/09/24.
+//
+
+import SwiftUI
+
+@main
+struct ConvercyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
